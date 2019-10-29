1a5c2a80633972a3778442b477a274d17883208e
Coty Getzelman <coty-crg@live.com>
2019-10-29 00:47:19 -0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectFolderButton = new System.Windows.Forms.Button();
+            this.SelectFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.PatchProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SelectFolderText = new System.Windows.Forms.TextBox();
+            this.OutputFileButton = new System.Windows.Forms.Button();
+            this.OutputFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.OutputFileText = new System.Windows.Forms.TextBox();
+            this.ApplyPatchButton = new System.Windows.Forms.Button();
+            this.Header = new System.Windows.Forms.Label();
+            this.SelectPatchFileButton = new System.Windows.Forms.Button();
+            this.PatchFileData = new System.Windows.Forms.TextBox();
+            this.SelectPatchFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.OutputText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SelectFolderButton
+            // 
+            this.SelectFolderButton.Location = new System.Drawing.Point(12, 49);
+            this.SelectFolderButton.Name = "SelectFolderButton";
+            this.SelectFolderButton.Size = new System.Drawing.Size(142, 89);
+            this.SelectFolderButton.TabIndex = 0;
+            this.SelectFolderButton.Text = "Select Input Folder";
+            this.SelectFolderButton.UseVisualStyleBackColor = true;
+            this.SelectFolderButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SelectFolderDialog
+            // 
+            this.SelectFolderDialog.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest_1);
+            // 
+            // PatchProgressBar
+            // 
+            this.PatchProgressBar.Location = new System.Drawing.Point(12, 360);
+            this.PatchProgressBar.Name = "PatchProgressBar";
+            this.PatchProgressBar.Size = new System.Drawing.Size(292, 23);
+            this.PatchProgressBar.TabIndex = 1;
+            this.PatchProgressBar.Click += new System.EventHandler(this.PatchProgressBar_Click);
+            // 
+            // SelectFolderText
+            // 
+            this.SelectFolderText.Location = new System.Drawing.Point(12, 144);
+            this.SelectFolderText.Name = "SelectFolderText";
+            this.SelectFolderText.Size = new System.Drawing.Size(142, 20);
+            this.SelectFolderText.TabIndex = 2;
+            this.SelectFolderText.TextChanged += new System.EventHandler(this.SelectFolderText_TextChanged);
+            // 
+            // OutputFileButton
+            // 
+            this.OutputFileButton.Location = new System.Drawing.Point(162, 49);
+            this.OutputFileButton.Name = "OutputFileButton";
+            this.OutputFileButton.Size = new System.Drawing.Size(142, 89);
+            this.OutputFileButton.TabIndex = 3;
+            this.OutputFileButton.Text = "Select Output File";
+            this.OutputFileButton.UseVisualStyleBackColor = true;
+            this.OutputFileButton.Click += new System.EventHandler(this.OutputFileButton_Click);
+            // 
+            // OutputFileText
+            // 
+            this.OutputFileText.Location = new System.Drawing.Point(162, 144);
+            this.OutputFileText.Name = "OutputFileText";
+            this.OutputFileText.Size = new System.Drawing.Size(142, 20);
+            this.OutputFileText.TabIndex = 4;
+            // 
+            // ApplyPatchButton
+            // 
+            this.ApplyPatchButton.Enabled = false;
+            this.ApplyPatchButton.Location = new System.Drawing.Point(12, 265);
+            this.ApplyPatchButton.Name = "ApplyPatchButton";
+            this.ApplyPatchButton.Size = new System.Drawing.Size(292, 89);
+            this.ApplyPatchButton.TabIndex = 5;
+            this.ApplyPatchButton.Text = "Apply Patch";
+            this.ApplyPatchButton.UseVisualStyleBackColor = true;
+            this.ApplyPatchButton.Click += new System.EventHandler(this.ApplyPatchButton_Click);
+            // 
+            // Header
+            // 
+            this.Header.AutoSize = true;
+            this.Header.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Header.Location = new System.Drawing.Point(23, 13);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(268, 33);
+            this.Header.TabIndex = 6;
+            this.Header.Text = "Translation Patcher";
+            this.Header.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SelectPatchFileButton
+            // 
+            this.SelectPatchFileButton.Location = new System.Drawing.Point(12, 170);
+            this.SelectPatchFileButton.Name = "SelectPatchFileButton";
+            this.SelectPatchFileButton.Size = new System.Drawing.Size(142, 89);
+            this.SelectPatchFileButton.TabIndex = 7;
+            this.SelectPatchFileButton.Text = "Select Patch File";
+            this.SelectPatchFileButton.UseVisualStyleBackColor = true;
+            this.SelectPatchFileButton.Click += new System.EventHandler(this.SelectPatchFileButton_Click);
+            // 
+            // PatchFileData
+            // 
+            this.PatchFileData.Location = new System.Drawing.Point(160, 170);
+            this.PatchFileData.Multiline = true;
+            this.PatchFileData.Name = "PatchFileData";
+            this.PatchFileData.Size = new System.Drawing.Size(142, 89);
+            this.PatchFileData.TabIndex = 8;
+            // 
+            // SelectPatchFileDialog
+            // 
+            this.SelectPatchFileDialog.FileName = "patch.txt";
+            // 
+            // OutputText
+            // 
+            this.OutputText.Enabled = false;
+            this.OutputText.Location = new System.Drawing.Point(12, 389);
+            this.OutputText.Multiline = true;
+            this.OutputText.Name = "OutputText";
+            this.OutputText.Size = new System.Drawing.Size(292, 154);
+            this.OutputText.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 555);
+            this.Controls.Add(this.OutputText);
+            this.Controls.Add(this.PatchFileData);
+            this.Controls.Add(this.SelectPatchFileButton);
+            this.Controls.Add(this.Header);
+            this.Controls.Add(this.ApplyPatchButton);
+            this.Controls.Add(this.OutputFileText);
+            this.Controls.Add(this.OutputFileButton);
+            this.Controls.Add(this.SelectFolderText);
+            this.Controls.Add(this.PatchProgressBar);
+            this.Controls.Add(this.SelectFolderButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectFolderButton;
+        private System.Windows.Forms.FolderBrowserDialog SelectFolderDialog;
+        private System.Windows.Forms.ProgressBar PatchProgressBar;
+        private System.Windows.Forms.TextBox SelectFolderText;
+        private System.Windows.Forms.Button OutputFileButton;
+        private System.Windows.Forms.SaveFileDialog OutputFileDialog;
+        private System.Windows.Forms.TextBox OutputFileText;
+        private System.Windows.Forms.Button ApplyPatchButton;
+        private System.Windows.Forms.Label Header;
+        private System.Windows.Forms.Button SelectPatchFileButton;
+        private System.Windows.Forms.TextBox PatchFileData;
+        private System.Windows.Forms.OpenFileDialog SelectPatchFileDialog;
+        private System.Windows.Forms.TextBox OutputText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SelectFolderDialog.ShowDialog();
+            
+            var path = SelectFolderDialog.SelectedPath;
+            SelectFolderText.Text = path;
+
+            TryEnableApplyButton();
+        }
+
+        private void SelectFolderText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OutputFileButton_Click(object sender, EventArgs e)
+        {
+            OutputFileDialog.AddExtension = true;
+            OutputFileDialog.DefaultExt = "iso";
+            OutputFileDialog.FileName = "patched.iso";
+            OutputFileDialog.Filter = "ISO file (*.iso)|*.iso)";
+
+            OutputFileDialog.ShowDialog();
+
+            var path = OutputFileDialog.FileName;
+            OutputFileText.Text = path;
+
+            TryEnableApplyButton(); 
+        }
+
+        private void TryEnableApplyButton()
+        {
+            var input = SelectFolderText.Text; 
+            var output = OutputFileText.Text;
+            var patchData = PatchFileData.Text;
+
+            var ready = !string.IsNullOrEmpty(input) 
+                && !string.IsNullOrEmpty(output) 
+                && !string.IsNullOrEmpty(patchData);
+
+            ApplyPatchButton.Enabled = ready; 
+        }
+
+        private void PatchProgressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ApplyPatchButton_Click(object sender, EventArgs e)
+        {
+            var inputPath = SelectFolderText.Text;
+            var outputPath = OutputFileText.Text;
+            var patchData = PatchFileData.Text;
+
+            DoApplyPatch(inputPath, outputPath, patchData);
+        }
+
+        private void SelectPatchFileButton_Click(object sender, EventArgs e)
+        {
+            SelectPatchFileDialog.ShowDialog();
+
+            var patchFilePath = SelectPatchFileDialog.FileName;
+            var patchData = System.IO.File.ReadAllText(patchFilePath);
+            PatchFileData.Text = patchData;
+
+            TryEnableApplyButton(); 
+        }
+        
+        private void DoApplyPatch(string inputFolder, string outputFile, string patchData)
+        {
+            PatchProgressBar.Minimum = 0;
+            PatchProgressBar.Maximum = 100;
+
+            // skip first line, its the header info 
+            var patches = patchData.Split('\n');
+            for(var p = 1; p < patches.Length; ++p) 
+            {
+                var patch = patches[p];
+                var data = patch.Split(',');
+
+                var original = data[0];
+                var translation = data[1];
+                
+                // enforce the translation to equal the number of bytes we're patching 
+                var originalBytes = Encoding.UTF8.GetBytes(original);
+                var translationBytes = Encoding.UTF8.GetBytes(translation); 
+
+                if(originalBytes.Length != translationBytes.Length)
+                {
+                    var cutTranslation = new byte[originalBytes.Length];
+                    for(var b = 0; b < cutTranslation.Length; ++b)
+                    {
+                        cutTranslation[b] = translationBytes[b];
+                    }
+                    translationBytes = cutTranslation;
+                }
+
+                var success = FindAndPatch(inputFolder, original, translation);
+            }
+
+        }
+
+        private bool FindAndPatch(string path, string searchTerm, string patch)
+        {
+            var directories = Directory.GetDirectories(path);
+            foreach (var directory in directories)
+            {
+                if (FindAndPatch(directory, searchTerm, patch))
+                {
+                    return true;
+                }
+            }
+
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                byte[] write_buffer = null;
+                bool found = false;
+                
+                using (var reader = File.OpenRead(file))
+                {
+                    var totalBytes = reader.Length;
+                    var read_buffer = new byte[totalBytes]; 
+
+                    var read_per = (int) Math.Min(totalBytes, 1024); 
+                    for(var b = 0; b < totalBytes - read_per; b += read_per)
+                    {
+                        reader.Read(read_buffer, b, read_per);
+                        PatchProgressBar.Value = (int) ((float) (b / totalBytes) * 100 / 2);
+                    }
+
+                    var dataString = Encoding.UTF8.GetString(read_buffer);
+                    var foundIndex = dataString.IndexOf(searchTerm);
+                    if(foundIndex >= 0)
+                    {
+                        dataString = dataString.Replace(searchTerm, patch);
+                        write_buffer = Encoding.UTF8.GetBytes(dataString);
+                        found = true; 
+                    }
+                }
+
+                if (found)
+                {
+                    // using (var writer = File.OpenWrite(file))
+                    // {
+                    //     var totalBytes = write_buffer.Length;
+                    //     var write_per = (int)Math.Min(totalBytes, 1024);
+                    //     for(var b = 0; b < totalBytes - write_per; b += write_per)
+                    //     {
+                    //         writer.Write(write_buffer, b, write_per);
+                    //         PatchProgressBar.Value = (int)((float)(b / totalBytes) * 100 / 2);
+                    //     }
+                    // }
+
+                    var log = string.Format($"Replaced \"{searchTerm}\" with \"{patch}\" at \"{path}\"");
+                    OutputText.AppendText(log);
+
+                    return false; 
+                }
+            }
+
+            return false;
+        }
+    }
+}
